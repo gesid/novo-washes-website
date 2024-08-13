3cdc2511586b3bfdeb9c1907adafd183ba63e20c
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-08-13 20:44:00 -0300

    feat: componente do footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import { FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FaXTwitter } from "react-icons/fa6";
+
+function Footer() {
+    return (
+        <>
+            <footer className="bg-[#1C1D21] text-white fixed bottom-0 w-full flex flex-col items-center py-6">
+                <img src="src/assets/imgs/logo_washes.png" alt="Logo" className="h-12 mb-4" />
+                <p className="mb-4">Siga nossas Redes Sociais</p>
+                <div className="flex space-x-6 mb-4">
+                    <a href="https://x.com" target="_blank" rel="noopener noreferrer" className="text-white">
+                        <FaXTwitter size={24} />
+                    </a>
+                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-white">
+                        <FaInstagram size={24} />
+                    </a>
+                    <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-white">
+                        <FaLinkedin size={24} />
+                    </a>
+                </div>
+                <p>© 2024 WASHES. Todos os direitos reservados.</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
