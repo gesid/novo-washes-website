dce6924605daaaa49a8ec66d0648f2d344769ad2
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-08-13 20:45:15 -0300

    criando rotas com suas devidas páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import Navbar from "./components/Navbar"
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import QuemSomos from './pages/QuemSomos';
+import Washes2024 from './pages/Washes2024';
+import EdicoesAnteriores from './pages/EdicoesAnteriores';
+import DataWashes from './pages/DataWashes';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <header>
-        <Navbar />
-      </header>
+      <Navbar />
+      <div className="pt-16">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/quem-somos" element={<QuemSomos />} />
+          <Route path="/washes-2024" element={<Washes2024 />} />
+          <Route path="/edicoes-anteriores" element={<EdicoesAnteriores />} />
+          <Route path="/datawashes" element={<DataWashes />} />
+          {/* <Route path="*" element={<NoMatch />} /> */}
+        </Routes>
+      </div>
+      <Footer />
     </>
   )
 }
