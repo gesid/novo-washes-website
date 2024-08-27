26dcfbabda89d563d8aad9163dc0401f42f9ff04
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-08-26 23:08:53 -0300

    feat: inserindo novo componente HeroWashes2024 na Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import HeroQuemSomos from "../components/HeroQuemSomos";
+import HeroWashes2024 from "../components/HeroWashes2024";
 
 function Home() {
     return (
         <div className="">
             <Hero />
             <HeroQuemSomos />
+            <HeroWashes2024 />
         </div>
     );
 };
