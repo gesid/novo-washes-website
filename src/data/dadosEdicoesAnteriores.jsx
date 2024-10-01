8e739067b6bf10141fcbe0f2a4cccab035b80608
Emanuel <emanuelcicero40@gmail.com>
2024-09-30 22:18:31 -0300

    feat: Componente de edições anteriores

---
+++ src/data/dadosEdicoesAnteriores.jsx
@@ -0,0 +1,77 @@
+import ImagemMaceio from "../assets/imgs/washes-i-maceio.png"
+import ImagemSalvador from "../assets/imgs/washes-ii-salvador.png"
+import ImagemNatal from "../assets/imgs/washes-iii-natal.png"
+import ImagemBelem from "../assets/imgs/washes-iv-belem.png"
+import ImagemRemoto from "../assets/imgs/washes-v-vi-remoto.png"
+import ImagemNiteroi from "../assets/imgs/washes-vii-niteroi.png"
+import ImagemJoaoPessoa from "../assets/imgs/washes-viii-joaoPessoa.png"
+
+export const dadosEdicoesAnteriores = [
+  { // Primeira Edição WASHES
+    imagem: ImagemMaceio,
+    edicao: "I WASHES",
+    local : "Maceió - Al",
+    anoDaEdicao: "(2016)",
+    linkWebsite: "",
+    linkAnalEvento: ""
+  },
+  { // Segunda Edição WASHES washes-II-Salvador
+    imagem: ImagemSalvador,
+    edicao: "II WASHES",
+    local : "Salvador - BA",
+    anoDaEdicao: "(2017)",
+    linkWebsite: "",
+    linkAnalEvento: ""
+  },
+  { // Terceira Edição WASHES
+    imagem: ImagemNatal,
+    edicao: "II WASHES",
+    local : "Natal - RN ",
+    anoDaEdicao: "(2018)",
+    linkWebsite: "",
+    linkAnalEvento: ""
+  },
+  { // Quarta Edição WASHES
+    imagem: ImagemBelem,
+    edicao: "IV WASHES",
+    local : "Belém - PA",
+    anoDaEdicao: "(2019)",
+    linkWebsite: "",
+    linkAnalEvento: ""
+  },
+  { // Quinta Edição WASHES
+    imagem: ImagemRemoto,
+    edicao: "V WASHES",
+    local : "Remoto",
+    anoDaEdicao: "(2020)",
+    linkWebsite: "",
+    linkAnalEvento: ""
+  },
+  { // Sexta Edição WASHES
+    imagem: ImagemRemoto,
+    edicao: "VI WASHES",
+    local : "Remoto",
+    anoDaEdicao: "(2021)",
+    linkWebsite: "",
+    linkAnalEvento: ""
+  },
+  { // Sétima Edição WASHES
+    imagem: ImagemNiteroi,
+    edicao: "VII WASHES",
+    local : "Niterói - RJ",
+    anoDaEdicao: "(2022)",
+    linkWebsite: "",
+    linkAnalEvento: ""
+  },
+  { // Oitava Edição WASHES
+    imagem: ImagemJoaoPessoa,
+    edicao: "VIII WASHES",
+    local : "João Pessoa - PB",
+    anoDaEdicao: "(2023)",
+    linkWebsite: "",
+    linkAnalEvento: ""
+  },
+  // Adicione edições futura abaixo
+]
+
+
--- src/pages/EdicoesAnteriores.jsx
+++ src/pages/EdicoesAnteriores.jsx
@@ -1,8 +1,20 @@
+import  { CardEdicoesAnteriores }from "../components/CardEdicoesAnteriores";
+import  { dadosEdicoesAnteriores } from "../data/dadosEdicoesAnteriores"
 
 const EdicoesAnteriores = () => {
   return (
-    <div>
-      <h1>EdicoesAnteriores</h1>
+    <div className="max-w-screen-2xl mx-auto flex flex-row flex-wrap px-4 justify-evenly">
+      {dadosEdicoesAnteriores.map((dadosEventos, index) => (
+        <CardEdicoesAnteriores
+          key={index}
+          imagem={dadosEventos.imagem}
+          edicao={dadosEventos.edicao}
+          local={dadosEventos.local}
+          anoDaEdicao={dadosEventos.anoDaEdicao}
+          linkWebsite={dadosEventos.linkWebsite}
+          linkAnalEvento={dadosEventos.linkAnalEvento}
+        />
+      ))}
     </div>
   );
 };
