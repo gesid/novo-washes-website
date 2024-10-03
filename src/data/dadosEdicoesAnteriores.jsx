1db00334e4ffb5b7a01fb1fbd62608c7ff1c6c27
Emanuel Cicero <110929438+EmanuelCicero@users.noreply.github.com>
2024-10-03 14:41:59 -0300

    feat: Componente de edições anteriores (#9)
    * feat: Componente de edições anteriores
    
    * refactor: Adicionando links para website e anais de eventos

---
+++ src/data/dadosEdicoesAnteriores.jsx
@@ -0,0 +1,77 @@
+import ImagemMaceio from "../assets/imgs/washes-i-maceio.png"
+import ImagemSalvador from "../assets/imgs/washes-ii-salvador.png"
+import ImagemNatal from "../assets/imgs/washes-iii-natal.png"
+import ImagemBelem from "../assets/imgs/washes-iv-belem.png"
+import ImagemRemoto from "../assets/imgs/washes-v-vi-remoto.png"
+import ImagemNiteroi from "../assets/imgs/washes-vii-niteroi.png"
+import ImagemJoaoPessoa from "../assets/imgs/washes-viii-joaoPessoa.png"
+
+export const dadosEdicoesAnteriores = [
+  { // Primeira Edição WASHES
+    imagem: ImagemMaceio,
+    edicao: "I WASHES",
+    local : "Maceió - Al",
+    anoDaEdicao: "(2016)",
+    linkWebsite: "http://reuse.cos.ufrj.br/washes2016/",
+    linkAnalEvento: "https://sol.sbc.org.br/index.php/washes/issue/view/369"
+  },
+  { // Segunda Edição WASHES washes-II-Salvador
+    imagem: ImagemSalvador,
+    edicao: "II WASHES",
+    local : "Salvador - BA",
+    anoDaEdicao: "(2017)",
+    linkWebsite: "http://reuse.cos.ufrj.br/washes-in-reuse-2017/",
+    linkAnalEvento: "https://sol.sbc.org.br/index.php/washes/issue/view/370"
+  },
+  { // Terceira Edição WASHES
+    imagem: ImagemNatal,
+    edicao: "II WASHES",
+    local : "Natal - RN ",
+    anoDaEdicao: "(2018)",
+    linkWebsite: "https://sites.google.com/view/washes2018/home",
+    linkAnalEvento: "https://sol.sbc.org.br/index.php/washes/issue/view/218"
+  },
+  { // Quarta Edição WASHES
+    imagem: ImagemBelem,
+    edicao: "IV WASHES",
+    local : "Belém - PA",
+    anoDaEdicao: "(2019)",
+    linkWebsite: "",
+    linkAnalEvento: "https://sol.sbc.org.br/index.php/washes/issue/view/387"
+  },
+  { // Quinta Edição WASHES
+    imagem: ImagemRemoto,
+    edicao: "V WASHES",
+    local : "Remoto",
+    anoDaEdicao: "(2020)",
+    linkWebsite: "",
+    linkAnalEvento: "https://sol.sbc.org.br/index.php/washes/issue/view/583"
+  },
+  { // Sexta Edição WASHES
+    imagem: ImagemRemoto,
+    edicao: "VI WASHES",
+    local : "Remoto",
+    anoDaEdicao: "(2021)",
+    linkWebsite: "https://csbc.ufsc.br/eventos/washes/",
+    linkAnalEvento: "https://sol.sbc.org.br/index.php/washes/issue/view/762"
+  },
+  { // Sétima Edição WASHES
+    imagem: ImagemNiteroi,
+    edicao: "VII WASHES",
+    local : "Niterói - RJ",
+    anoDaEdicao: "(2022)",
+    linkWebsite: "",
+    linkAnalEvento: "https://sol.sbc.org.br/index.php/washes/issue/view/932"
+  },
+  { // Oitava Edição WASHES
+    imagem: ImagemJoaoPessoa,
+    edicao: "VIII WASHES",
+    local : "João Pessoa - PB",
+    anoDaEdicao: "(2023)",
+    linkWebsite: "https://csbc.sbc.org.br/2023/washes/",
+    linkAnalEvento: "https://sol.sbc.org.br/index.php/washes/issue/view/1116"
+  },
+  // Adicione edições futuras abaixo
+]
+
+
