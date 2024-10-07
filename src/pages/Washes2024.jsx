1e5a4483a4a53396ba31a97952846305b01201e0
Emanuel <emanuelcicero40@gmail.com>
2024-10-07 11:10:09 -0300

    feat: Componente banner WASHES 2024

--- src/pages/Washes2024.jsx
+++ src/pages/Washes2024.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Card from "../components/Card";
 import ComitePrograma from "../components/ComitePrograma";
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+import { BannerWASHES2024 } from "../components/bannerWASHES2024";
 
 const Washes2024 = () => {
   
@@ -67,61 +68,64 @@ const Washes2024 = () => {
   ];
 
   return (
-    <div className="container mx-auto px-4 py-10">
-      {/* Membros Permanentes */}
-      <h1 className="text-center text-2xl font-bold mb-8">Membros Permanentes</h1>
-      <div className="flex justify-center flex-wrap gap-6 mb-12">
-        {members.map((member, index) => (
-          <Card
-            key={index}
-            imgSrc={member.imgSrc}
-            name={member.name}
-            university={member.university}
-            memberType={member.memberType}
-            linkedinUrl={member.linkedinUrl}
-            lattesUrl={member.lattesUrl}
-          />
-        ))}
-      </div>
-
-      {/* Coordenação 2024 */}
-      <h2 className="text-center text-2xl font-bold mb-8">Coordenação 2024</h2>
-      <div className="flex justify-center gap-2">
-        {coordinators.map((coordinator, index) => (
-          <Card
-            key={index}
-            imgSrc={coordinator.imgSrc}
-            name={coordinator.name}
-            university={coordinator.university}
-            memberType={coordinator.memberType}
-            linkedinUrl={coordinator.linkedinUrl}
-            lattesUrl={coordinator.lattesUrl}
-          />
-        ))}
-      </div>
-
-      {/* Dropdown Comitê de Programa */}
-      <div className="mt-8 text-center">
-        <div
-          className="flex justify-center items-center cursor-pointer"
-          onClick={toggleDropdown} // Alterna o dropdown ao clicar na div
-        >
-          <h2 className="text-2xl font-bold mx-2">Comitê de Programa</h2>
-          {isDropdownOpen ? <SlArrowUp /> : <SlArrowDown />}
+    <section>
+      <BannerWASHES2024/>
+      <div className="container mx-auto px-4 py-10">
+        {/* Membros Permanentes */}
+        <h1 className="text-center text-2xl font-bold mb-8">Membros Permanentes</h1>
+        <div className="flex justify-center flex-wrap gap-6 mb-12">
+          {members.map((member, index) => (
+            <Card
+              key={index}
+              imgSrc={member.imgSrc}
+              name={member.name}
+              university={member.university}
+              memberType={member.memberType}
+              linkedinUrl={member.linkedinUrl}
+              lattesUrl={member.lattesUrl}
+            />
+          ))}
         </div>
 
-        {/* Transição suave do conteúdo do dropdown */}
-        <div
-          className={`transition-all duration-500 ease-in-out overflow-hidden ${
-            isDropdownOpen ? "max-h-full opacity-100" : "max-h-0 opacity-0"
-          }`}
-        >
-          <div className={`mt-4 ${isDropdownOpen ? "block" : "hidden"}`}>
-            <ComitePrograma />
+        {/* Coordenação 2024 */}
+        <h2 className="text-center text-2xl font-bold mb-8">Coordenação 2024</h2>
+        <div className="flex justify-center gap-2">
+          {coordinators.map((coordinator, index) => (
+            <Card
+              key={index}
+              imgSrc={coordinator.imgSrc}
+              name={coordinator.name}
+              university={coordinator.university}
+              memberType={coordinator.memberType}
+              linkedinUrl={coordinator.linkedinUrl}
+              lattesUrl={coordinator.lattesUrl}
+            />
+          ))}
+        </div>
+
+        {/* Dropdown Comitê de Programa */}
+        <div className="mt-8 text-center">
+          <div
+            className="flex justify-center items-center cursor-pointer"
+            onClick={toggleDropdown} // Alterna o dropdown ao clicar na div
+          >
+            <h2 className="text-2xl font-bold mx-2">Comitê de Programa</h2>
+            {isDropdownOpen ? <SlArrowUp /> : <SlArrowDown />}
+          </div>
+
+          {/* Transição suave do conteúdo do dropdown */}
+          <div
+            className={`transition-all duration-500 ease-in-out overflow-hidden ${
+              isDropdownOpen ? "max-h-full opacity-100" : "max-h-0 opacity-0"
+            }`}
+          >
+            <div className={`mt-4 ${isDropdownOpen ? "block" : "hidden"}`}>
+              <ComitePrograma />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
