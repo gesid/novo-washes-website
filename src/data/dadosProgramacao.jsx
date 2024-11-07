e3d7395a1eeebadd036c4f87d280387ed58d2140
Emanuel Cicero <110929438+EmanuelCicero@users.noreply.github.com>
2024-11-07 18:13:05 -0300

    feat: Componente programação (#16)

---
+++ src/data/dadosProgramacao.jsx
@@ -0,0 +1,172 @@
+
+export const dadosProgramacao = [
+  { 
+    data: "(23/07)",
+    horarioDeInicio: "8:30 AM",
+    apresentador:"Chair: Williamson Silva (UNIPAMPA) ",
+    sessao: "ST-1: Sessão Técnica 1",
+    programacao:[
+      {
+        horarioApresentacao: "8:30 - 8:45",
+        temaApresentacao: "Abertura - Introdução ao WASHES"
+      },
+      {
+        horarioApresentacao: "8:45 - 9:00",
+        temaApresentacao: "Kanban Board Guide for Startups: introduzindo boas práticas para melhorar a experiência do desenvolvedor Angelica Santos (UFSCAR), Erik Gabriel Rodrigo da Silva (UFSCAR), Luciana Zaina (UFSCAR)"
+      },
+      {
+        horarioApresentacao: "9:00 - 9:15",
+        temaApresentacao: "Investigando a Prática de Salas de Guerra na Solução de Problemas Críticos em Sistemas de SoftwareAlexandre Cysne Esteves (PPGI/UFRJ), Luis Moraes (PPGI/UFRJ), Rafael Maiani de Mello (UFRJ)"
+      },
+      {
+        horarioApresentacao: "9:15 - 9:30",
+        temaApresentacao: "Caracterizando Condutores de Carga Cognitiva na Prática de Testes UnitáriosBruno Barroso (UFRJ), Leonardo César da Silva (UFRJ), Rafael Maiani de Mello (UFRJ)",
+      },
+      {
+        horarioApresentacao: "9:30 - 9:45",
+        temaApresentacao: "Percepções Iniciais de Estudantes da Graduação sobre a LGPD e Sistemas de InformaçãoLucas Moura (UFC), Emanuel Coutinho (UFC)"
+      },
+      
+    ]
+
+  },
+  {
+    data: "(23/07)",
+    horarioDeInicio: "9:45 AM",
+    apresentador:"Chair: Emanuel Coutinho (UFC)",
+    sessao: "ST-2: Sessão Técnica 2",
+    programacao:[
+      {
+        horarioApresentacao: "9:45 - 10:00",
+        temaApresentacao: "Impactos de Uma Capacitação no Desenvolvimento de Habilidades Socioemocionais, Acadêmicas e Profissionais de Desenvolvedoras de SoftwaresLeticia de Paula (UFAC), Laura Costa Sarkis (UFAC), Daricélio Moreira Soares (Federal University of Acre), Daniel Silva (UFAC)      "
+      },
+      {
+        horarioApresentacao: "10:00 - 10:15",
+        temaApresentacao: "dataWASHES - Towards an Application Programming Interface of WASHES proceedings dataAllysson Allex Araújo (Universidade Federal do Cariri), João Isaac Alves Farias (Universidade Federal do Cariri), José Victor Lima Gonçalves (Universidade Federal do Cariri), Rodrigo Pereira dos Santos (UNIRIO), Davi Viana (UFMA), Igor Steinmacher (NAU)"
+      },
+      {
+        horarioApresentacao: "9:15 - 9:30",
+        temaApresentacao: "O que sabemos sobre Testes em Chatbots? uma revisão sistemática da literaturaGabriel Santos (UTFPR), Williamson Silva (UNIPAMPA), Pedro Henrique Dias Valle (UFJF)"
+      },
+
+    ]
+
+  },
+  {
+    data: "(23/07)",
+    horarioDeInicio: "11:00 AM",
+    apresentador:"Chair: Pedro Henrique Dias Valle (UFJF)",
+    sessao: "ST-3: Sessão Técnica 3",
+    programacao:[
+      {
+        horarioApresentacao: "11:00 - 11:15",
+        temaApresentacao: "Os Impactos Financeiros dos Jogos NFT durante o Período Pandêmico no Cenário BrasileiroHugo Patricio, Alan Gomes, Emanuel Coutinho (UFC)"
+      },
+      {
+        horarioApresentacao: "11:15 - 11:30",
+        temaApresentacao: "What has the WASHES community discussed about Software Economics in the last years?Allysson Allex Araújo (Universidade Federal do Cariri), José Victor Lima Gonçalves (Universidade Federal do Cariri), João Isaac Alves Farias (Universidade Federal do Cariri), Rodrigo Pereira dos Santos (UNIRIO), Davi Viana (UFMA)"
+      },
+      {
+        horarioApresentacao: "11:30 - 11:45",
+        temaApresentacao: "Gestão de Custos em Computação em Nuvem: um estudo sobre desperdícios financeiros e estratégias de correçãoZaira Souza (USP), Thiago Adriano Coleti (UENP), Renato Balancieri (UNESPAR), Marcelo Morandini (USP)"
+      },
+      {
+        horarioApresentacao: "11:45 - 12:00",
+        temaApresentacao:"Investigando a experiência do usuário com um chatbot de atendimento ao cliente: um caso com o software de uma operadora de telecomunicaçõesAna Paula Pereira Lopes de Carvalho, Leo Natan Paschoal (ICMC/USP)"
+      }
+
+    ]
+
+  },
+  {
+    data: "(23/07)",
+    horarioDeInicio: "12:00 PM",
+    apresentador:"Chair: Juliana Saraiva (UFPB)",
+    sessao: "ST-4: Sessão Técnica 4",
+    programacao:[
+      {
+        horarioApresentacao: "12:00 - 12:15",
+        temaApresentacao:"Desenvolvimento de Software com Fins Sociais: Relato de Experiência em Projetos de Extensão UniversitáriaDenise Stringhini (UNIFESP), Daniel Salis, Danilo Alexandre, Milena Siqueira, Felipe Guerra (UNIFESP), Tiago Oliveira (UNIFESP)"
+      },
+      {
+        horarioApresentacao: "12:15 - 12:30",
+        temaApresentacao:"Um Estudo Comparativo entre a Visão de Líderes e Liderados sobre a Importância de Soft Skills em Desenvolvimento de SoftwareMurilo Coelho (UECE), Allysson Allex Araújo (Universidade Federal do Cariri), Sávio Freire (Federal Institute of Ceara), Matheus Paixão (UECE)"
+      },
+      {
+        horarioApresentacao: "12:30 - 12:45",
+        temaApresentacao:"Investigating the Participation of Female Software Developers in Retrospective MeetingsLarissa Galeno (UFRJ/COPPE/PESC), Wesley Mota de Oliveira Gomes (UFRJ), Rafael Maiani de Mello (UFRJ)"
+      },
+      {
+        horarioApresentacao: "12:45 - 13:00",
+        temaApresentacao:"Uma Proposta de Framework Multimodal para Avaliação de Experiência de Usuário em Sistemas Web InterativosRodrigo Oliveira Zacarias (UNIRIO), Thiago Parracho (UNIRIO), Flávio Moura (UFPA), Danilo T. Lima (UFPA), Suzane Santos dos Santos (University of São Paulo), Kamila Rios da Hora Rodrigues (USP), Marcos César da Rocha Seruffo (UFPA), Rodrigo Pereira dos Santos (UNIRIO)"
+      },
+
+    ]
+
+  },
+  {
+    data: "(23/07)",
+    horarioDeInicio: "16:30 PM",
+    apresentador:"Chair: Chair: Rodrigo Santos (UNIRIO)",
+    sessao: "Painel BraSNAM-WASHES",
+    programacao:[
+      {
+        horarioApresentacao: "16:30 - 17:30",
+        temaApresentacao:"Painel BraSNAM-WASHES - Tema: Oportunidades de Pesquisa, Avanço e Aproximação das Comunidades: Carla Silva (UFPE) Daniel Kaster (UEL) Davi Viana (UFMA) Jonice Oliveira (UFRJ) Valdemar Graciano Neto (UFG)"
+      },
+
+    ]
+
+  },
+  {
+    data: "(23/07)",
+    horarioDeInicio: "17:30 PM",
+    apresentador:"",
+    sessao: "Fórum dos Grandes Desafios de Pesquisa em Aspectos Sociais, Humanos e Econômicos de Software (I GranDASHES-BR)",
+    programacao:[
+      {
+        horarioApresentacao: "17:30 - 17:35",
+        temaApresentacao:"Understanding and measuring software engineer behavior: What can we learn from the behavioral sciences?Allysson Allex Araújo (Universidade Federal do Cariri), Marcos Kalinowski (PUC-RIO), Daniel Graziotin (University of Hohenheim) "
+      },
+      {
+        horarioApresentacao: "17:35 - 17:40",
+        temaApresentacao:"Software Engineering Competency ChallengesAdriana Silveira de Souza (PUC Goiás), Juliano Lopes de Oliveira (UFG), Sofia Costa Paiva (UFG), Alexandre Marcos Lins de Vasconcelos (UFPE)"
+      },  {
+        horarioApresentacao: "17:40 - 17:45",
+        temaApresentacao:"Grandes Desafios para Diversidade, Equidade e Inclusão (DEI) no Desenvolvimento de SoftwareAnna Beatriz Marques (UFC), Carla Bezerra (UFC), Marina Rocha (UFC), Davi Viana (UFMA)"
+      },  {
+        horarioApresentacao: "17:45 - 17:50",
+        temaApresentacao:"Desafios de Compliance da LGPD: Implantação na Indústria de Software BrasileiraJuliana de Albuquerque Saraiva (Federal University of Paraíba), Cleidson de Souza (UFPA), Sergio Soares (UFPE)"
+      },  {
+        horarioApresentacao: "17:50 - 17:55",
+        temaApresentacao:"LGPD e Requisitos de Software: Desafios e Oportunidades de PesquisaLucas Moura (UFC), Emanuel Coutinho (UFC)"
+      },  {
+        horarioApresentacao: "17:55 - 18:00",
+        temaApresentacao:"Modelagem e Simulação: Um Grande Desafio para Aspectos Humanos, Sociais e Econômicos de SoftwareValdemar Vicente Graciano Neto (UFG), Emanuel Coutinho (UFC)"
+      },  {
+        horarioApresentacao: "18:00 - 18:30",
+        temaApresentacao:"Discussão com a comunidade + Encerramento + Premiação"
+      },
+
+    ]
+
+  },
+  // Adicione mais dias e apresentações abaixo
+  // {
+  //   data: "",
+  //   horarioDeInicio: "",
+  //   apresentador:"",
+  //   sessao: "",
+  //   programacao:[
+  //     {
+  //       horarioApresentacao: "",
+  //       temaApresentacao:""
+  //     },
+
+  //   ]
+
+  // },
+  
+  
+]
+
