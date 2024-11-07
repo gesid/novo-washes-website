df2036cd538743cb7d52537bdd28b97d9679be84
Emanuel Cicero <110929438+EmanuelCicero@users.noreply.github.com>
2024-11-07 18:13:05 -0300

    feat: Componente programação (#16)

--- src/pages/Washes2024.jsx
+++ src/pages/Washes2024.jsx
@@ -5,7 +5,10 @@ import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 import { BannerWASHES2024 } from "../components/bannerWASHES2024";
 import { TopicoDeInteresse } from "../components/topicoDeInteresse";
 import { ChamadaDeTrabalhos } from "../components/ChamadaDeTrabalhos";
+import { Programacao } from "../components/Programacao";
 import { dadosChamadaDeTrabalho } from "../data/dadosChamadaDeTrabalhos";
+import { dadosProgramacao } from "../data/dadosProgramacao";
+
 
 const Washes2024 = () => {
   
@@ -73,6 +76,23 @@ const Washes2024 = () => {
   return (
     <section>
       <BannerWASHES2024/>
+      
+      <div className="max-w-screen-xl mx-auto text-[#2f2f2f] flex flex-col lg:gap-5 gap-2 lg:px-16 md:px-10 px-5 my-20">
+        <h1 className="font-bold lg:text-3xl text-2xl">Programação</h1>
+        {/* <div className="w-full bg-[#2A5D82] text-white flex items-center p-2 text-3xl rounded-lg">
+          <p>Terça-Feira (23/07)</p>
+        </div> */}
+        {dadosProgramacao.map((dados, index) => (
+          <Programacao
+            key={index}
+            data={dados.data}
+            horarioDeInicio={dados.horarioDeInicio}
+            apresentador={dados.apresentador}
+            sessao={dados.sessao}
+            programacao={dados.programacao}
+          />
+        ))}
+      </div>
       <TopicoDeInteresse/>
 
       <div className="max-w-screen-xl my-16 mx-auto py-2 flex flex-col gap-5 lg:px-16 md:px-10 px-5 text-[#2f2f2f]">
