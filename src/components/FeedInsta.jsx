383baf88609ff78e15dedb290c7232fc39e66928
Emanuel Cicero <110929438+EmanuelCicero@users.noreply.github.com>
2024-11-18 17:07:59 -0300

    refactor: Atualizando design do componente de redes sociais (#18)

--- src/components/FeedInsta.jsx
+++ src/components/FeedInsta.jsx
@@ -1,69 +1,47 @@
-import { Link } from "react-router-dom";
-import { FaInstagram } from "react-icons/fa";
+import { RiBlueskyLine } from "react-icons/ri";
+import { FaInstagram,  FaXTwitter, FaLinkedinIn } from "react-icons/fa6";
 
 function FeedInsta() {
     return (
         <>
-            <section className="w-full h-fit py-3.5 px-5 flex flex-col items-center justify-center gap-5 lg:px-5">
-                <p className="text-3xl mt-20 mb-14 lg:hidden">
-                    Nos acompanhe no <span className="text-[#E72B78]">Instagram</span>
-                </p>
-                <p className="hidden text-4xl text-[#717275] mt-11 pl-11 mb-8 lg:w-full lg:flex">
+            <section className="w-full h-fit py-3.5 px-5 flex flex-col items-center justify-center gap-2 lg:px-2 lg:mt-24 mt-10 mb-10">
+                <p className="lg:text-4xl text-3xl text-center text-[#003358]">
                     Acompanhe-nos nas{" "}
-                    <span className="pl-1 font-bold">Redes Sociais</span>!
+                    <span className="font-bold">Redes Sociais!</span>
                 </p>
-                <div className="w-full h-fit flex flex-row gap-2.5 lg:gap-9 lg:h-96">
-                    <Link
-                        to={"https://www.instagram.com/p/CvuaOlSOLaB/"}
-                        target="_blank"
-                        className="w-full h-32 bg-feed-insta-1 bg-no-repeat bg-cover bg-left rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:w-1/2 lg:h-96 lg:rounded-2xl"
+                <div className="container w-full flex flex-wrap gap-4 justify-evenly text-[#2f2f2f] font-bold sm:text-xl text-lg lg:m-14 md:m-7 m-10">
+                    <a href="https://x.com/washescommunity" 
+                        target="_blank" 
+                        className="lg:border-none border border-[#2f2f2f3b] flex items-center justify-center gap-2 lg:hover:bg-[#dadadabe] py-2 px-4 sm:w-auto w-full rounded-full duration-300 transition ease-in-out hover:-translate-y-1 hover:scale-100 "> <FaXTwitter size={25}/> <p>@WASHESCOMMUNITY</p></a>
+                    <a href="https://www.instagram.com/washescommunity" 
+                        target="_blank" 
+                        className="lg:border-none border border-[#2f2f2f3b] flex items-center justify-center gap-2 lg:hover:bg-[#dadadabe] py-2 px-4 sm:w-auto w-full rounded-full duration-300 transition ease-in-out hover:-translate-y-1 hover:scale-100"> <FaInstagram size={25}/> <p> @WASHESCOMMUNITY</p></a>
+                    <a href="https://www.linkedin.com/in/washescommunity/" 
+                        target="_blank" 
+                        className="lg:border-none border border-[#2f2f2f3b] flex items-center justify-center gap-2 lg:hover:bg-[#dadadabe] py-2 px-4 sm:w-auto w-full rounded-full duration-300 transition ease-in-out hover:-translate-y-1 hover:scale-100"> <FaLinkedinIn size={25}/> <p> @WASHESCOMMUNITY</p></a>                    
+                    <a href="https://bsky.app/profile/washescommunity.bsky.social" 
+                        target="_blank" 
+                        className="lg:border-none border border-[#2f2f2f3b] flex items-center justify-center gap-2 lg:hover:bg-[#dadadabe] py-2 sm:w-auto w-full px-4 rounded-full duration-300 transition ease-in-out hover:-translate-y-1 hover:scale-100"> <RiBlueskyLine size={25}/> <p> @WASHESCOMMUNITY</p></a>
+                </div>
+                <div className="w-full h-fit flex flex-row gap-2 lg:h-96">
+                    <div
+                    className="w-full h-32 bg-feed-insta-1 bg-no-repeat bg-cover bg-center rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:w-1/2 lg:h-96 lg:rounded-2xl"
                     />
-                    <Link
-                        to={"https://www.instagram.com/p/CvuaOlSOLaB/?img_index=2"}
-                        target="_blank"
-                        className="w-full h-32 bg-feed-insta-2 bg-no-repeat bg-cover bg-left-top rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:w-1/2 lg:h-96 lg:rounded-2xl"
+                    <div className="w-full h-32 bg-feed-insta-2 bg-no-repeat bg-cover bg-center rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:w-1/2 lg:h-96 lg:rounded-2xl"
                     />
                 </div>
-                <Link
-                    to={"https://www.instagram.com/p/CvuaOlSOLaB/?img_index=2"}
-                    target="_blank"
-                    className="w-full h-44 bg-feed-insta-3 bg-no-repeat bg-cover bg-top rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:hidden"
-                />
-                <div className="w-full h-fit flex flex-row gap-2.5 lg:gap-9 lg:h-96">
-                    <Link
-                        to={"https://www.instagram.com/p/CvuaOlSOLaB/?img_index=2"}
-                        target="_blank"
-                        className="hidden bg-feed-insta-3 bg-no-repeat bg-cover bg-center rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:block lg:w-1/3 lg:h-96"
+
+                <div className="w-full h-44 bg-feed-insta-3 bg-no-repeat bg-cover bg-center rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:hidden"/>
+
+                <div className="w-full h-fit flex flex-row gap-2 lg:h-96">
+                    <div className="hidden bg-feed-insta-3 bg-no-repeat bg-cover bg-center rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:block lg:w-1/3 lg:h-96"
                     />
-                    <Link
-                        to={"https://www.instagram.com/p/C61p4o5unGk/"}
-                        target="_blank"
-                        className="w-1/2 h-52 bg-feed-insta-4 bg-no-repeat bg-cover bg-center rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:w-1/3 lg:h-96 lg:rounded-2xl"
+                    <div className="w-1/2 h-52 bg-feed-insta-4 bg-no-repeat bg-cover bg-center rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:w-1/3 lg:h-96 lg:rounded-2xl"
                     />
-                    <Link
-                        to={"https://www.instagram.com/p/CvuaOlSOLaB/?img_index=5"}
-                        target="_blank"
-                        className="w-1/2 h-52 bg-feed-insta-5 bg-no-repeat bg-cover bg-center rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:w-1/3 lg:h-96 lg:rounded-2xl"
+                    <div className="w-1/2 h-52 bg-feed-insta-5 bg-no-repeat bg-cover bg-center rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)] lg:w-1/3 lg:h-96 lg:rounded-2xl"
                     />
                 </div>
-                <Link
-                    to={"https://www.instagram.com/washescommunity/"}
-                    target="_blank"
-                    className="flex items-center justify-center gap-2.5 px-4 py-2 my-9
-                    bg-[#E72B78] rounded-lg shadow-[4px_6px_6px_0px_rgba(0,0,0,0.18)]
-                    hover:border hover:border-spacing-[0.25px] hover:border-[#E72B78] hover:bg-white hover:text-[#E72B78]
-                    cursor-pointer transition-colors duration-300
-                    text-base text-white uppercase"
-                >
-                    <FaInstagram className="text-2xl lg:text-4xl" />
-                    <p className="lg:text-4xl">@washescommunity</p>
-                </Link>
             </section>
-            <div className="flex lg:hidden w-full h-1 flex-row gap-3 justify-evenly">
-                <div className="w-full h-1 bg-[#E72B78]"></div>
-                <div className="w-full h-1 bg-[#36BCEE]"></div>
-                <div className="w-full h-1 bg-[#66C75C]"></div>
-            </div>
         </>
     );
 }
