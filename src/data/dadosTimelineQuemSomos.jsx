488ba4a6b04de14dbac50935665638d57b1634f0
Emanuel Cicero <110929438+EmanuelCicero@users.noreply.github.com>
2024-11-25 12:28:23 -0300

    Refactor na timeline quem somos (#19)

---
+++ src/data/dadosTimelineQuemSomos.jsx
@@ -0,0 +1,57 @@
+export const dadosTimelineQuemSomos = [
+  {
+    year: "2024",
+    title: "Nona edição",
+    description:
+      `Realizado em Brasília, o WASHES teve 21 artigos publicados. </br> <u><a href="https://sol.sbc.org.br/index.php/washes/issue/view/1323" target="_blank"> Saiba mais! </a></u> `,
+  },
+  {
+    year: "2023",
+    title: "Oitava edição",
+    description:
+      `Realizado em João Pessoa, o WASHES teve 12 artigos publicados. </br> <u><a href="https://sol.sbc.org.br/index.php/washes/issue/view/1116" target="_blank"> Saiba mais! </a></u> `,
+  },
+  {
+    year: "2022",
+    title: "Sétima edição",
+    description:
+      `Realizado em Niterói, o WASHES teve 7 artigos publicados. </br> <u><a href="https://sol.sbc.org.br/index.php/washes/issue/view/932" target="_blank"> Saiba mais! </a></u> `,
+  },
+  {
+    year: "2021",
+    title: "Sexta edição",
+    description:
+      `Realizado em Remoto, o WASHES teve 7 artigos publicados. </br> <u><a href="https://sol.sbc.org.br/index.php/washes/issue/view/762" target="_blank"> Saiba mais! </a></u> `,
+  },
+  {
+    year: "2020",
+    title: "Quinta edição",
+    description:
+      `Realizado em Remoto, o WASHES teve 14 artigos publicados. </br> <u><a href="https://sol.sbc.org.br/index.php/washes/issue/view/583" target="_blank"> Saiba mais! </a></u> `,
+  },
+  {
+    year: "2019",
+    title: "Quarta edição",
+    description:
+      `Realizado em Belém, o WASHES teve 11 artigos publicados. </br> <u><a href="https://sol.sbc.org.br/index.php/washes/issue/view/387" target="_blank"> Saiba mais! </a></u> `,
+  },
+  {
+    year: "2018",
+    title: "Terceira edição",
+    description:
+      `Realizado em Natal, o WASHES teve 11 artigos publicados. </br> <u><a href="https://sol.sbc.org.br/index.php/washes/issue/view/218" target="_blank"> Saiba mais! </a></u> `,
+  },
+  {
+    year: "2017",
+    title: "Segunda edição",
+    description:
+      `Realizado em Salvador, o WASHES teve 4 artigos publicados. </br> <u><a href="https://sol.sbc.org.br/index.php/washes/issue/view/370" target="_blank"> Saiba mais! </a></u> `,
+  },
+  {
+    year: "2016",
+    title: "Primeira edição",
+    description:
+      `Realizado em Maceió, o WASHES teve 15 artigos publicados. </br> <u><a href="https://sol.sbc.org.br/index.php/washes/issue/view/369" target="_blank"> Saiba mais! </a></u> `,
+  },
+  
+]
