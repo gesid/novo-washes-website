2242c769b832bc1968fc9ae4176d9bbf2a61ebd6
Emanuel Cicero <110929438+EmanuelCicero@users.noreply.github.com>
2024-11-28 08:51:45 -0300

    Ajustando responsividade da home e organização das imagens de banner em uma pasta (#20)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,12 @@ import { Link } from "react-scroll";
 function Hero() {
     return (
         <>
-            <section className="w-full h-96 flex flex-col items-center justify-center lg:h-dvh lg:bg-banner-hero lg:bg-no-repeat lg:bg-cover lg:bg-center
-            sm:bg-banner-hero sm:bg-no-repeat sm:bg-cover sm:bg-center">
+            <section className="w-full h-96 flex gap-2 flex-row items-center justify-center lg:h-dvh lg:bg-banner-hero lg:bg-no-repeat lg:bg-cover lg:bg-center
+            sm:bg-banner-hero sm:bg-no-repeat sm:bg-cover sm:bg-center relative overflow-hidden">
+                <img className="w-[512px] sm:hidden absolute top-[-10%] left-[25%] opacity-15" src="src/assets/imgs/logo_washes.svg" alt="" />
                 <div className="flex flex-col items-center justify-center mx-5 gap-16 lg:gap-32">
                     <div className="max-w-screen-xl w-full flex flex-col items-center justify-between mx-5 text-center">
-                        <h2 className="text-2xl mb-3 text-gray-500 font-semibold lg:hidden">
+                        <h2 className="text-2xl mb-3 text-gray-600 font-semibold lg:hidden">
                             WASHES
                         </h2>
                         <h1 className="text-2xl font-bold text-zinc-800 mx-5 lg:text-6xl">
@@ -31,7 +32,7 @@ function Hero() {
                     </div>
                 </div>
             </section>
-            <div className="flex lg:hidden w-full h-1 flex-row gap-3 justify-evenly mt-8">
+            <div className="flex sm:hidden w-full h-1 flex-row gap-3 justify-evenly mt-8">
                 <div className="w-full h-1 bg-[#E72B78]"></div>
                 <div className="w-full h-1 bg-[#36BCEE]"></div>
                 <div className="w-full h-1 bg-[#66C75C]"></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,9 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "banner-hero": "url(src/assets/imgs/banner-hero-home.svg)",
-        "banner-hero-home-quemSomos": "url(src/assets/imgs/banner-hero-home-quemSomos.png)",
-        'banner-hero-quemSomos': "url(src/assets/imgs/banner-hero-quemSomos.png)",
-        "banner-hero-washes2024": "url(src/assets/imgs/banner-hero-home-washes2024.png)",
+        "banner-hero": "url(src/assets/banners/banner-hero-home.svg)",
         // Altere o caminho da nova foto do local do próximo evento abaixo
-        "banner-hero-imagembannerWASHES202*":"url(src/assets/imgs/banner-hero-imagembannerWASHES2024.png)",
+        "banner-hero-imagembannerWASHES202*":"url(src/assets/banners/banner-hero-imagembannerWASHES2024.png)",
         "feed-insta-1": "url(src/assets/feedInstaImages/img001.png)",
         "feed-insta-2": "url(src/assets/feedInstaImages/img002.png)",
         "feed-insta-3": "url(src/assets/feedInstaImages/img003.png)",
