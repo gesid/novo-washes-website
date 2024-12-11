7aaef57e387f1323d22ce6daa4acc05440e71d1b
Gustavo Ferreira Reinaldo <141689100+gustavo-reinaldo@users.noreply.github.com>
2024-12-10 23:39:26 -0300

    Mudanças para o ano de 2025 (#25)
    Co-authored-by: Gustavo Reinaldo <gustavo.reinaldo@hp.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import QuemSomos from './pages/QuemSomos';
-import Washes2024 from './pages/Washes2024';
+import Washes2025 from './pages/Washes2025';
 import EdicoesAnteriores from './pages/EdicoesAnteriores';
 import DataWashes from './pages/DataWashes';
+import Washes2024 from './pages/Washes2024';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/quem-somos" element={<QuemSomos />} />
-          <Route path="/washes-2024" element={<Washes2024 />} />
+          <Route path="/washes-2024" element={<Washes2024/>} />
+          <Route path="/washes-2025" element={<Washes2025/>} />
           <Route path="/edicoes-anteriores" element={<EdicoesAnteriores />} />
           <Route path="/datawashes" element={<DataWashes />} />
           {/* <Route path="*" element={<NoMatch />} /> */}
---
+++ src/components/HeroWashes2025.jsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+
+function HeroWashes2025() {
+    return (
+        <>
+            <section id="heroQuemSomos" className="container mx-auto flex flex-col mt-8 mb-8 lg:my-24 lg:flex-row px-5">
+                <div className="flex flex-col items-center lg:flex-row gap-7">
+                    <div>
+                        <img className="lg:max-w-md md:max-w-sm" src="../src/assets/banners/banner-hero-home-washes2024.png" alt="Ilustração de pessoas trabalhando com dados e gráficos" />
+                        <div className='text-right pr-2'>
+                            <p className="lg:text-7xl text-5xl text-[#6C757D] opacity-50 font-medium">WASHES</p>
+                            <p className="lg:text-7xl text-5xl text-[#6C757D] opacity-50 font-light italic">2025</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center text-center mx-5 gap-7 lg:gap-8 lg:items-start lg:text-left">
+                    <h1 className="text-[28px] lg:text-[32px] font-semibold text-[#0D6080]">WASHES 2025</h1>
+                    <p className="text-lg font-light text-[#2F2F2F] lg:text-[26px] leading-9 lg:text-justify">
+                        O WASHES 2025 será realizado no formato presencial nos dias 22 e 23 de julho de 2025 (datas sujeitas à mudanças), como um evento satélite do <strong className="font-bold">45o. Congresso da Sociedade Brasileira de Computação (SBC)</strong>, que será realizado entre 20 e 24 de julho de 2025.
+                        </p>
+                        <Link
+                            to="/quem-somos"
+                            className="px-4 py-2 flex justify-center bg-[#74C76B] hover:bg-white
+                            text-base text-white hover:text-[#74C76B] hover:outline hover:outline-1 cursor-pointer
+                            rounded transition-colors duration-300 lg:px-5 lg:py-3 lg:text-2xl"
+                        >
+                            Saiba mais sobre o WASHES 2025!
+                        </Link>
+                    </div>
+                </div>
+            </section>
+            <div className="flex lg:hidden w-full h-1 flex-row gap-3 justify-evenly">
+                <div className="w-full h-1 bg-[#E72B78]"></div>
+                <div className="w-full h-1 bg-[#36BCEE]"></div>
+                <div className="w-full h-1 bg-[#66C75C]"></div>
+            </div>
+        </>
+    );
+}
+
+export default HeroWashes2025;
