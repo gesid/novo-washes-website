a012ef8614ba5acd2ed833f43f182e197b6847ed
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-12-11 00:42:57 -0300

    fix: another import image error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <>
             <footer className="bg-[#1C1D21] text-white bottom-0 w-full flex flex-col items-center py-6">
-                <img src="..//assets/imgs/logo_washes.svg" alt="Logo" className="h-12 mb-4" />
+                <img src="/assets/imgs/logo_washes.svg" alt="Logo" className="h-12 mb-4" />
                 <p className="mb-4">Siga nossas Redes Sociais</p>
                 <div className="flex space-x-6 mb-4">
                     <a href="https://x.com" target="_blank" rel="noopener noreferrer" className="text-white">
