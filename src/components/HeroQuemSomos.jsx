406b6720451c215643d745755356cdcc73901638
Emanuel Cicero <110929438+EmanuelCicero@users.noreply.github.com>
2024-12-10 22:16:57 -0300

    refactor: Padronizando os tamanhos das fontes (#24)

--- src/components/HeroQuemSomos.jsx
+++ src/components/HeroQuemSomos.jsx
@@ -6,8 +6,8 @@ function HeroQuemSomos() {
             <section id="heroQuemSomos" className="container mx-auto w-full flex flex-col items-center justify-center mt-10 mb-8 lg:my-24 lg:flex-row px-5">
                 <div className="flex flex-col gap-7 justify-center items-center lg:flex-row">
                     <div className="flex flex-col items-center text-center mx-5 gap-7 lg:gap-8 lg:items-start lg:text-left">
-                        <h1 className="text-3xl lg:text-4xl font-semibold text-[#0D6080]">QUEM SOMOS?</h1>
-                        <p className="text-lg font-light text-[#2F2F2F] lg:text-3xl">
+                        <h1 className="text-[28px] lg:text-[32px] font-semibold text-[#0D6080]">QUEM SOMOS?</h1>
+                        <p className="text-lg font-light text-[#2F2F2F] lg:text-[26px] leading-9">
                             O WASHES é um workshop que aborda a interação entre fatores <strong className="font-bold">humanos</strong>, <strong className="font-bold">sociais</strong> e <strong className="font-bold">econômicos</strong> na <strong className="font-bold">Engenharia de Software</strong>, promovendo discussões sobre seu impacto no desenvolvimento e uso de software, bem como colaborações com a indústria.
                         </p>
                         <Link
